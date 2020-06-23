0840d061ed3ee835d1a92297745a0bf8e4098f14
unknown <Wu.Liverpool@sjtu.edu.cn>
2020-06-23 22:51:14 +0800

    add decoder

--- model/transformer/transformer.py
+++ model/transformer/transformer.py
@@ -1,15 +1,9 @@
 import torch
 import torch.nn as nn
 
-# class Transformer(nn.Module):
-#     """Transformer"""
-#
-#     def __init__(self):
-#         super(Transformer, self).__init__()
 
-
-class Encoder(nn.Module):
-    """Encoder of the Transformer model
+class Transformer(nn.Module):
+    """Transformer
 
     Simplified:
         layers: 6
@@ -17,44 +11,24 @@ class Encoder(nn.Module):
         layers of feed-forward networks: 1
     """
 
-    def __init__(self, input_size, hid_size, dropout=0.7, max_len=100):
-        super(Encoder, self).__init__()
+    def __init__(self, encoder, decoder):
+        super(Transformer, self).__init__()
 
-        self.n = 6
-        self.h = 8
-        self.d = torch.sqrt(torch.FloatTensor([hid_size]))
+        self.encoder = encoder
+        self.decoder = decoder
 
-        self.tok_embedding = nn.Embedding(input_size, hid_size)
-        self.pos_embedding = nn.Embedding(max_len, hid_size)
-        self.self_attention = MultiHeadAttention(hid_size, self.h)
-        self.ffn = nn.Linear(hid_size, hid_size)
-        self.layer_norm = nn.LayerNorm(hid_size)
-        self.dropout = nn.Dropout(dropout)
+    @staticmethod
+    def get_trg_mask(trg):
+        trg_len = trg.shape[1]
+        return torch.tril(torch.ones((trg_len, trg_len))).bool()
 
-    def forward(self, src, src_mask=None):
-        batch_size, src_len = src.shape
+    def forward(self, src, trg):
+        encoder_output = self.encoder(src)
 
-        # Token embeddings
-        te = self.tok_embedding(src) * self.d
-        # Positional embeddings
-        pos = torch.arange(0, src_len).unsqueeze(0).repeat(batch_size, 1)
-        pe = self.pos_embedding(pos)
-        src = te + pe
-        src = self.dropout(src)
+        trg_mask = self.get_trg_mask(trg)
+        prediction = self.decoder(trg, encoder_output, trg_mask)
 
-        # Stacked transformer
-        # multi-head attention layer
-        # add & layer normalization
-        # feed-forward networks
-        # add & layer normalization
-        for i in range(self.n):
-            attention_output = self.self_attention(src, src, src, src_mask)
-            attention_output = self.layer_norm(src + self.dropout(attention_output))
-
-            encoder_output = self.ffn(attention_output)
-            encoder_output = self.layer_norm(attention_output + self.dropout(encoder_output))
-
-        return encoder_output
+        return prediction
 
 
 class MultiHeadAttention(nn.Module):
@@ -98,3 +72,97 @@ class MultiHeadAttention(nn.Module):
         y = y.permute(0, 2, 1, 3).contiguous().view(batch_size, -1, hid_size)
 
         return self.w_o(y)
+
+
+class Encoder(nn.Module):
+    """Encoder of the Transformer model"""
+
+    def __init__(self, input_size, hid_size, dropout=0.7, max_len=100):
+        super(Encoder, self).__init__()
+
+        self.n = 6
+        self.h = 8
+        self.d = torch.sqrt(torch.FloatTensor([hid_size]))
+
+        self.tok_embedding = nn.Embedding(input_size, hid_size)
+        self.pos_embedding = nn.Embedding(max_len, hid_size)
+        self.self_attention = MultiHeadAttention(hid_size, self.h)
+        self.ffn = nn.Linear(hid_size, hid_size)
+        self.layer_norm = nn.LayerNorm(hid_size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, src, src_mask=None):
+        batch_size, src_len = src.shape
+
+        # Token embeddings
+        te = self.tok_embedding(src) * self.d
+        # Positional embeddings
+        pos = torch.arange(0, src_len).unsqueeze(0).repeat(batch_size, 1)
+        pe = self.pos_embedding(pos)
+        src = te + pe
+        src = self.dropout(src)
+
+        # Stacked transformer
+        # multi-head attention layer
+        # add & layer normalization
+        # feed-forward networks
+        # add & layer normalization
+        for i in range(self.n):
+            attention_output = self.self_attention(src, src, src, src_mask)
+            attention_output = self.layer_norm(src + self.dropout(attention_output))
+
+            encoder_output = self.ffn(attention_output)
+            encoder_output = self.layer_norm(attention_output + self.dropout(encoder_output))
+
+        return encoder_output
+
+
+class Decoder(nn.Module):
+    """Decoder of the Transformer model"""
+
+    def __init__(self, output_size, hid_size, dropout=0.7, max_len=100):
+        super(Decoder, self).__init__()
+
+        self.n = 6
+        self.h = 8
+        self.d = torch.sqrt(torch.FloatTensor([hid_size]))
+
+        self.tok_embedding = nn.Embedding(output_size, hid_size)
+        self.pos_embedding = nn.Embedding(max_len, hid_size)
+        self.self_attention = MultiHeadAttention(hid_size, self.h)
+        self.ffn = nn.Linear(hid_size, hid_size)
+        self.layer_norm = nn.LayerNorm(hid_size)
+        self.dropout = nn.Dropout(dropout)
+        self.fc_out = nn.Linear(hid_size, output_size)
+
+    def forward(self, trg, encoder_output, trg_mask, src_mask=None):
+        batch_size, trg_len = trg.shape
+
+        # Token embeddings
+        te = self.tok_embedding(trg) * self.d
+        # Positional embeddings
+        pos = torch.arange(0, trg_len).unsqueeze(0).repeat(batch_size, 1)
+        pe = self.pos_embedding(pos)
+        trg = te + pe
+        trg = self.dropout(trg)
+
+        # Stacked transformer
+        # multi-head attention layer
+        # add & layer normalization
+        # feed-forward networks
+        # add & layer normalization
+        # encoder-decoder attention layer
+        # linear & softmax
+        for i in range(self.n):
+            attention_output = self.self_attention(trg, trg, trg, trg_mask)
+            attention_output = self.layer_norm(trg + self.dropout(attention_output))
+
+            attention_output = self.self_attention(attention_output, encoder_output, encoder_output, src_mask)
+            attention_output = self.layer_norm(attention_output + self.dropout(attention_output))
+
+            decoder_output = self.ffn(attention_output)
+            decoder_output = self.layer_norm(attention_output + self.dropout(decoder_output))
+
+        prediction = torch.softmax(self.fc_out(decoder_output), dim=-1)
+
+        return prediction
